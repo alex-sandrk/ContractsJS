0b4df39627c4b87f3cd89ec88603610cfafd4c50
Kodee McIntosh <kodee@titan.io>
2022-02-11 09:47:43 -0700

    pulling eslint attempt number 3

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 'use strict';
 
 const mapValues = require('modify-values');
-const { Qtum } = require('qtumjs');
 
 const chainAliases = require('./aliases.json');
 const contractsDefinition = require('./contracts.json');
@@ -11,11 +10,13 @@ const allContracts = mapValues(chainAliases, function (chainAlias) {
   const definitions = contractsDefinition[chainAlias];
   return mapValues(
     definitions.contracts,
-    ({ address, birthblock, version }, name) => ({
-      abi: require(`./abis/${version || definitions.version}/${name}.json`),
-      address,
-      birthblock: birthblock || definitions.birthblock
-    })
+    ({ address, birthblock, version }, name) => {
+      return {
+        abi: require(`./abis/${version || definitions.version}/${name}.json`).abi,
+        address,
+        birthblock: birthblock || definitions.birthblock
+      };
+    }
   );
 });
 
@@ -28,18 +29,10 @@ const allContracts = mapValues(chainAliases, function (chainAlias) {
  * @returns {*} The contract instance.
  */
 
-const createContract = function (web3, abi, address, qtumrpc) {
-  let contract;
-  if (qtumrpc) {
-    const contractsRepo = { contracts: { '': { abi, address } } };
-    const qtum = new Qtum(qtumrpc, contractsRepo);
-    contract = qtum.contract('');
-  } else {
-    contract = typeof web3.eth.Contract === 'function'
-      ? new web3.eth.Contract(abi, address)
-      : web3.eth.contract(abi).at(address);
-  }
-  return contract;
+const createContract = function (web3, abi, address) {
+  return typeof web3.eth.Contract === 'function'
+    ? new web3.eth.Contract(abi, address)
+    : web3.eth.contract(abi).at(address);
 };
 
 /** Class representing a contracts set. */
@@ -52,22 +45,20 @@ class LumerinContracts {
    *
    * @param {Web3} web3 The Web3 instance to instantiate the contracts.
    * @param {string} [chain='mainnet'] The target chain name or ID.
-   * @param {string} qtumrpc qtum rpc url
    */
-  constructor (web3, chain = 'mainnet', qtumrpc) {
+  constructor (web3, chain = 'mainnet') {
     if (!web3 || !web3.eth) {
-      throw new Error('Invalid web3 or qtumrpc provided');
+      throw new Error('Invalid web3 provided');
     }
 
-    // const contracts = allContracts[chainAliases[chain]];
-    const contracts = allContracts[chain];
+    const contracts = allContracts[chainAliases[chain]];
 
     if (!contracts) {
       throw new Error('Unknown chain');
     }
 
     Object.assign(this, mapValues(contracts, ({ abi, address }) =>
-      createContract(web3, abi, address, qtumrpc)
+      createContract(web3, abi, address)
     ));
   }
 }
